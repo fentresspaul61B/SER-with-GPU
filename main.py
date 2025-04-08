22e1eb1ed3fcf69ea87564b5d8f58c76f13ba468
fentresspaul61B <77761564+fentresspaul61B@users.noreply.github.com>
2025-04-07 18:06:27 -0700

    wrong repo, trying again.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, HTTPException
 # from fastapi import HTTPException
 # import subprocess
 import tempfile
@@ -7,6 +7,7 @@ import os
 # import torch
 import time
 from model import SerModel
+import torch
 
 MODEL = SerModel()
 
@@ -19,6 +20,13 @@ def save_upload_file_to_temp(upload_file: UploadFile) -> str:
         shutil.copyfileobj(upload_file.file, temp_file)
         temp_file_path = temp_file.name
     return temp_file_path
+
+
+@app.post("/check-gpu/")
+async def check_gpu():
+    if not torch.cuda.is_available():
+        raise HTTPException(status_code=400, detail="CUDA is not available")
+    return {"cuda": True}
 
 
 @app.post("/predict/")
